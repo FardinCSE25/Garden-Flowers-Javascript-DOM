ef08a5d06d485d90bc603d16ee0024aefa7b1631
Fardin Ahmed <fardin.cse25@gmail.com>
2025-09-23 20:09:30 +0600

    fully updated

--- README.md
+++ README.md
@@ -1,41 +1,43 @@
-1) What is the difference between var, let, and const?
-Answer :
-* var : It is global scope variable declarer if it is declared outside a function. Any variable can be redeclared and reassigned by var and is initialized with undefined.
+1. What is the difference between var, let, and const?
+   Answer :
 
-* const : It is a block scope variable declarer or the variable declared by const can't be accessed before initialization. Any variable can't be redeclared and can't be reassigned by const.
+- var : It is global scope variable declarer if it is declared outside a function. Any variable can be redeclared and reassigned by var and is initialized with undefined.
 
-* let : It is also a block scope variable declarer like const. That's why the variable can't be accessed before initialization. Any variable can't be redeclared but can be reassigned by let.
+- const : It is a block scope variable declarer or the variable declared by const can't be accessed before initialization. Any variable can't be redeclared and can't be reassigned by const.
 
-2) What is the difference between map(), forEach(), and filter()?
-Answer :
-* map() : Map is a type of loop which makes changes in an array's elements by calling a function and returns a new array with the changed elements.
+- let : It is also a block scope variable declarer like const. That's why the variable can't be accessed before initialization. Any variable can't be redeclared but can be reassigned by let.
 
-* forEach() : It is also a loop as like map which also makes changes in the elements of any array. But the difference is forEach doesn't return anything.
+2. What is the difference between map(), forEach(), and filter()?
+   Answer :
 
-* filter() : It is a type of loop which filters (selects or eliminates) the elements of array by imposing any condition on them. and returns an array with the filtered elements.
+- map() : Map is a type of loop which makes changes in an array's elements by calling a function and returns a new array with the changed elements.
 
-3) What are arrow functions in ES6?
-Answer :
-As we know, the function is a process of doing functional task by creating a scope around it. But the arrow function is different in case of ES6. Arrow function is shorter then the regular function. In arrow functions, it is not necessary to use "function" word before naming it. we can just name it and set a parameter and use '=>' this arrow sign and conduct the tasks. If the function is inline or single task conducting then it doesn't need to be returned and no need of { }. As example,
+- forEach() : It is also a loop as like map which also makes changes in the elements of any array. But the difference is forEach doesn't return anything.
 
-* Regular function :
-const function Something(Parameter){
-        <!-- operations -->
-}
+- filter() : It is a type of loop which filters (selects or eliminates) the elements of array by imposing any condition on them. and returns an array with the filtered elements.
 
-* Arrow function :
-const Something => (the single operation)
-multi task conducting :
-const Something => {
-        <!-- multi operations -->
-     return
-}
+3. What are arrow functions in ES6?
+   Answer :
+   As we know, the function is a process of doing functional task by creating a scope around it. But the arrow function is different in case of ES6. Arrow function is shorter then the regular function. In arrow functions, it is not necessary to use "function" word before naming it. we can just name it and set a parameter and use '=>' this arrow sign and conduct the tasks. If the function is inline or single task conducting then it doesn't need to be returned and no need of { }. As example,
 
-4) How does destructuring assignment work in ES6?
-Answer :
-Destruction of assignment is mainly used in arrays and objects to access their elements or key's values putting them into separate individual variables.
+- Regular function :
+  const function Something(Parameter){
+  <!-- operations -->
+  }
 
-5) Explain template literals in ES6. How are they different from string concatenation?
-Answer :
-When we feel necessity of using more symbols in one set quotation (""), then we use backticks, it is called template literals. Because in normal strings we can't create any whitespaces between two other words. then we have to use + to joining a white space between them. like "'word' + ' ' + 'word'" .
-But this problem is solved by template literals. we can easily create spaces or adding different types of symbol inside the backtick. like `<h1> I am Fardin </h1>` 
+- Arrow function :
+  const Something => (the single operation)
+  multi task conducting :
+  const Something => {
+  <!-- multi operations -->
+  return
+  }
+
+4. How does destructuring assignment work in ES6?
+   Answer :
+   Destruction of assignment is mainly used in arrays and objects to access their elements or key's values putting them into separate individual variables.
+
+5. Explain template literals in ES6. How are they different from string concatenation?
+   Answer :
+   When we feel necessity of using more symbols in one set quotation (""), then we use backticks, it is called template literals. Because in normal strings we can't create any whitespaces between two other words. then we have to use + to joining a white space between them. like "'word' + ' ' + 'word'" .
+   But this problem is solved by template literals. we can easily create spaces or adding different types of symbol inside the backtick. like `<h1> I am Fardin </h1>`
--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const showCategoriesContainer = document.getElementById("showCategories");
 const showPlants = document.getElementById("showAllPlants");
 const cartContainer = document.getElementById("cartContainer");
 const totalAmount = document.getElementById("totalAmount");
+const total = Number(document.getElementById("totalAmount").innerText)
+console.log(total);
 
 let cart = [];
 
@@ -60,7 +62,7 @@ const loadPlantsByCategory = (id) => {
 const showPlantByCategory = (plants) => {
   showPlants.innerHTML = "";
   plants.forEach((plant) => {
-    showPlants.innerHTML += `<div id='${plants.id}' class="plantDiv p-4 bg-white rounded-lg mb-3">
+    showPlants.innerHTML += `<div id='${plant.id}' class="plantDiv p-4 bg-white rounded-lg mb-3">
               <img class="rounded-lg mb-3 h-[280px] w-full" src="${plant.image}" alt="${plant.name}">
               <h3 onclick="loadPlantDetail(${plant.id})" class="inline cursor-pointer hover:bg-[#DCFCE7] hover:text-[#15803D] text-sm font-semibold">${plant.name}</h3>
               <p class="text-xs h-[70px] mt-2 mb-2">${plant.description}</p>
@@ -68,7 +70,7 @@ const showPlantByCategory = (plants) => {
                 <h3 class="bg-[#DCFCE7] rounded-[400px] text-sm text-[#15803D] px-3 py-1">${plant.category}</h3>
                 <p class="text-sm font-semibold"><span> ৳ </span><span class="price">${plant.price}</span></p>
               </div>
-              <button class="w-full text-center rounded-[999px] bg-[#15803D] text-white py-3">Add to Cart</button>
+              <button onclick="addToCart(this)" class="w-full text-center rounded-[999px] bg-[#15803D] text-white py-3">Add to Cart</button>
             </div>`;
   });
   manageLoading(false);
@@ -84,11 +86,13 @@ const loadPlantDetail = (id) => {
 };
 
 // load cart detail
-showPlants.addEventListener("click", (e) => {
-  if (e.target.innerText === "Add to Cart") {
-    const name = e.target.parentNode.children[1].innerText;
-    const id = e.target.parentNode.id;
-    const price = e.target.parentNode;
+   const addToCart = (btn) => {
+    let totalPrice = Number(document.getElementById("totalAmount").innerText)
+    // console.log(totalPrice);
+    
+    const name = btn.parentNode.children[1].innerText;
+    const id = btn.parentNode.id;
+    const price = btn.parentNode;
     const newPrice = price.querySelector(".price").innerText;
     // console.log({newPrice})
     cart.push({
@@ -96,15 +100,25 @@ showPlants.addEventListener("click", (e) => {
       price: newPrice,
       id: id,
     });
+    totalPrice = totalPrice + Number(newPrice)
+    console.log(totalPrice);
+    
     showCart(cart);
-  }
-});
+    showTotal(totalPrice)
+};
+
+const showTotal = (price) => {
+  totalAmount.innerText = price
+}
 // show cart
 const showCart = (cart) => {
-  console.log(cart);
-  
+  // console.log(cart);
+
   cartContainer.innerHTML = "";
+
   cart.forEach((cartDetail) => {
+    // console.log(cartDetail);
+    
     cartContainer.innerHTML += `<div class="bg-[#F0FDF4] px-3 py-2 mb-1 flex justify-between items-center rounded-lg">
                 <div>
                   <h1 class="mb-1 text-sm font-semibold">${cartDetail.name}</h1>
@@ -115,21 +129,43 @@ const showCart = (cart) => {
                 </div>
               </div>`;
 
-    let total = parseInt(totalAmount.innerText);
-    console.log(cartDetail.price);
-    
-    finalAmount = total + Number(cartDetail.price);
-    totalAmount.innerText = parseInt(finalAmount);
+    // console.log(cartDetail.price);
 
+    // let total = 0;
+    // total = parseInt(totalAmount.innerText);
+    // cart.forEach((item) => {
+    //   total += Number(item.price);
+    // });
+    // totalAmount.innerText = total;
+    // //   for (let index = 0; index < cart.length; index++) {
+    // // }
+    // // const element = cart[cart.length-1];
+    // //   console.log(element.price);
+    // //   finalAmount = total + Number(element.price);
+    // //   totalAmount.innerText = parseInt(finalAmount);
   });
 };
 // delete cart
-const deleteCart = (id) => {
-  // console.log(id)
-  const filteredCarts = cart.filter((cartDetail) => cartDetail.id != id);
-  cart = filteredCarts;
+// delete cart
+const deleteCart = (cartId) => {
+  // যেটা delete করতে হবে সেটাকে cart থেকে খুঁজে বের করি
+  const deletedItem = cart.find((item) => item.id == cartId);
+
+  if (deletedItem) {
+    // total থেকে ওই item's price বাদ দেই
+    let totalPrice = Number(totalAmount.innerText);
+    totalPrice -= Number(deletedItem.price);
+
+    totalAmount.innerText = totalPrice;
+  }
+
+  // নতুন cart বানাই item বাদ দিয়ে
+  cart = cart.filter((cartDetail) => cartDetail.id != cartId);
+
+  // cart আবার দেখাই
   showCart(cart);
 };
+
 
 const showPlantDetail = (detail) => {
   // console.log(detail);
@@ -144,12 +180,6 @@ const showPlantDetail = (detail) => {
       </div>`;
   document.getElementById("plantDetail").showModal();
 };
-
-// const showModal = (plantName) => {
-// showPlants.addEventListener("click", (e) => {
-//   if(e.target === plantName)
-//   console.log(plantName)
-// })};
 
 // manage loading
 const manageLoading = (status) => {
@@ -189,15 +219,7 @@ const showAllPlants = (allPlants) => {
                 <h3 class="bg-[#DCFCE7] rounded-[400px] text-sm text-[#15803D] px-3 py-1">${plants.category}</h3>
                 <p class="text-sm font-semibold"><span>৳</span><span class="price">${plants.price}</span></p>
               </div>
-              <button class="w-full text-center rounded-[999px] bg-[#15803D] text-white py-3">Add to Cart</button>
+              <button onclick="addToCart(this)" class="w-full text-center rounded-[999px] bg-[#15803D] text-white py-3">Add to Cart</button>
             </div>`;
-    //        let total = totalAmount.innerText
-    //       console.log(total)
-    //       console.log(cartDetail.price)
-    // finalAmount = parseInt(total + plants.price)
-    // // console.log(finalAmount)
-    // totalAmount.innerText = finalAmount
-
-    // console.log(category)
   });
 };
